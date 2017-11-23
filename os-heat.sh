942b9c19f40dfe2cc60816bdb0c61cd4871dee72
root <ali.sanhaji@orange.com>
2017-11-23 12:00:21 +0100

    Adding Heat template

--- os-heat.sh
+++ os-heat.sh
@@ -21,6 +21,9 @@ openstack role add --project service --user heat admin
 openstack service create --name heat \
   --description "Orchestration" orchestration
 
+openstack service create --name heat-cfn \
+  --description "Orchestration"  cloudformation
+
 openstack endpoint create --region RegionOne \
   orchestration public http://controller:8004/v1/%\(tenant_id\)s
 
@@ -29,6 +32,15 @@ openstack endpoint create --region RegionOne \
 
 openstack endpoint create --region RegionOne \
   orchestration admin http://controller:8004/v1/%\(tenant_id\)s
+
+openstack endpoint create --region RegionOne \
+  cloudformation public http://controller:8000/v1
+
+openstack endpoint create --region RegionOne \
+  cloudformation internal http://controller:8000/v1
+
+openstack endpoint create --region RegionOne \
+  cloudformation admin http://controller:8000/v1
 
 openstack domain create --description "Stack projects and users" heat
 
@@ -43,7 +55,8 @@ openstack role add --project demo --user demo heat_stack_owner
 openstack role create heat_stack_user
 
 # Installing Heat services
-yum install -y openstack-heat-api openstack-heat-engine
+yum install -y openstack-heat-api openstack-heat-api-cfn \
+  openstack-heat-engine
 
 cp conf/heat/heat.conf /etc/heat/heat.conf
 
@@ -52,7 +65,7 @@ su -s /bin/sh -c "heat-manage db_sync" heat
 
 # Starting Heat service
 systemctl enable openstack-heat-api.service \
-  openstack-heat-engine.service
+  openstack-heat-api-cfn.service openstack-heat-engine.service
 
 systemctl start openstack-heat-api.service \
-  openstack-heat-engine.service
+  openstack-heat-api-cfn.service openstack-heat-engine.service
